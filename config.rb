7cb166607604dbed9b463c5e660d8d09ea48ba24
Scott Nix <scott@scottnix.com>
2014-03-29 18:31:27 -0700

    Update for clarity

--- config.rb
+++ config.rb
@@ -1,16 +1,18 @@
 # compass config.rb refrences - http://compass-style.org/help/tutorials/configuration-reference/
 
-# require susy grids - http://susy.oddbird.net/
-require "susy"
-
 # path to file structures
-css_dir = ""
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+http_path = "/"
+css_dir = "/"
+sass_dir = "/scss"
+images_dir = "/images"
+fonts_dir = "/fonts"
+javascripts_dir = "/js"
 
 # output_style options - :nested, :expanded, :compact, :compressed
 output_style = :expanded
 
 # line_comments options - true, false
 line_comments = false
+
+# require susy grids - http://susy.oddbird.net/
+require "susy"
