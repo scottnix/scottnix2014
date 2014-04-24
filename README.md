aeb8c10add2a66aa99732294c253a26e28fc65d1
Scott Nix <scott@scottnix.com>
2014-04-24 01:36:25 -0700

    Force Scrollbar
    For edge case issues, stops really short pages from appearing to jump
    when the scrollbar is not displayed

---
+++ README.md
@@ -0,0 +1,31 @@
+ScottNix, a child theme for the Thematic Theme Framework
+===============
+
+A child theme for Thematic 2.0, this is a WIP, there is no design yet, just updating everything to current standards in anticipation for the release of Thematic 2.0. This will eventually power my website and also be available for download.
+
+
+Todo
+-------------
+
+* Update Susy grids to latest standards, drop old syntax.
+* Incorporate Tim's Emwaw (http://emwaw.me/) to detach some of the styling/naming issues that are present from creating themes meant to be modified.
+* Create a new design, will be somewhat similar to old design, possibly even more simple.
+* Spend way too much time picking a friggin color scheme.
+ * Incorporate Autoprefixer, couldn't get it running in Compass, but eventually need to swap from using Compass to vendor prefix things to Autoprefixer which is much more robust at keeping with current prefixes.
+
+Demo
+-------------
+
+http://scottnix.com/ - not up yet
+
+More Info
+-------------
+
+None Yet, saving docs for last.
+
+Contribute
+--------------
+
+Anything to add? Improvements? Create an issue on GitHub.
+
+https://github.com/scottnix/scottnix2014/issues
---
+++ functions.php
@@ -0,0 +1,405 @@
+<?php
+
+/**
+ * Notes
+ *
+ * Optiing to use Modernizr.... but just load shim, that way if you do want additional functionality you can just replace the file with a custom build...
+ * WP Screenshot PNG size changed http://codex.wordpress.org/Theme_Development#Screenshot
+ *
+ */
+
+
+/**
+ * Remove Parent Styling
+ *
+ * Removes the default Thematic CSS styling completely.
+ *
+ */
+
+function childtheme_remove_parent_styling( $dependencies ) {
+    return array();
+}
+add_filter( 'thematic_childtheme_style_dependencies', 'childtheme_remove_parent_styling' );
+
+/**
+ * Remove Thematic Menu JavaScript
+ *
+ * Removes the default Thematic JS scripts (Superfish) completely.
+ *
+ * Reference http://thematictheme.com/forums/topic/correct-way-to-prevent-loading-thematic-scripts/
+ *
+ */
+
+function childtheme_remove_superfish(){
+    remove_theme_support('thematic_superfish');
+ }
+add_action('wp_enqueue_scripts','childtheme_remove_superfish', 9);
+
+/**
+ * Script Manager
+ *
+ * Setup for adding and removing scripts and styling.
+ *
+ * Reference http://wpcandy.com/teaches/how-to-load-scripts-in-wordpress-themes/
+ *
+ */
+
+function childtheme_script_manager() {
+    // wp_register_script template ( $handle, $src, $deps, $ver, $in_footer );
+
+    // registers modernizr script, childtheme path, no dependency, no version, loads in header
+    wp_register_script('modernizr-js', get_stylesheet_directory_uri() . '/js/modernizr.js', false, false, false);
+    // registers misc custom script, childtheme path, yes dependency is jquery, no version, loads in footer
+    wp_register_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', array('jquery'), false, true);
+
+    // enqueue the scripts for use in theme
+    wp_enqueue_script ('modernizr-js');
+    wp_enqueue_script('custom-js');
+}
+add_action('wp_enqueue_scripts', 'childtheme_script_manager');
+
+// deregister styles
+function childtheme_deregister_styles() {
+    // removes contact form 7 styling
+    wp_deregister_style('contact-form-7');
+    // remove like 2 bullshit classes from jetpack on a individual CSS file
+    wp_deregister_style('jetpack-widgets');
+}
+add_action('wp_print_styles', 'childtheme_deregister_styles', 100);
+
+// deregister scripts
+function childtheme_deregister_scripts() {
+    // removes themaitc-js which has more Superfish scripts
+    wp_dequeue_script('thematic-js');
+    // remove some bs gravatar script jetpack loads
+    wp_deregister_script('devicepx');
+
+    if ( ! is_page('contact') ) {
+        // remove contact form7 styles on all pages but contact page
+        wp_dequeue_script('contact-form-7');
+     }
+}
+add_action( 'wp_print_scripts', 'childtheme_deregister_scripts', 100 );
+
+/**
+ * Modernizr add 'no-js'.
+ *
+ * This filter adds the 'no-js' class to the HTML tag, which Modernizr will remove (if Javascript is enabled) and replace it with a "js" class. This is super useful for providing CSS fallbacks, but Modernizr does a ton more.
+ *
+ * Reference http://modernizr.com/docs/
+ *
+ */
+
+function childtheme_html_class( $class_att ) {
+    $class_att = "no-js";
+    return $class_att;
+}
+add_filter( 'thematic_html_class', 'childtheme_html_class' );
+
+/**
+ * Add Favicon
+ *
+ * The Favicon is actually really complicated, but a quick and dirty method is to at least add a 32x32 ico file (at the least).
+ *
+ * Reference http://www.jonathantneal.com/blog/understand-the-favicon/
+ * Photoshop Plugin to save ICO files http://www.telegraphics.com.au/sw/
+ */
+
+function childtheme_add_favicon() { ?>
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+<?php }
+add_action('wp_head', 'childtheme_add_favicon');
+
+/**
+ * Clean up <head> of Site
+ *
+ * Wordpress by default throws in all kinds of relational links, for SEO purposes, sometimes they work, and sometimes they don't. A Plugin like WordPress SEO can handle some of these also, but others are not included.
+ *
+ * Reference http://scottnix.com/polishing-thematics-head/
+ *
+ */
+
+// remove really simple discovery
+remove_action('wp_head', 'rsd_link');
+// remove windows live writer xml
+remove_action('wp_head', 'wlwmanifest_link');
+// remove index relational link
+remove_action('wp_head', 'index_rel_link');
+// remove parent relational link
+remove_action('wp_head', 'parent_post_rel_link');
+// remove start relational link
+remove_action('wp_head', 'start_post_rel_link');
+// remove prev/next relational link
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+// remove short link
+remove_action('wp_head', 'wp_shortlink_wp_head');
+
+/**
+ * Register Two Additional Menus
+ *
+ * Not always needed, but a lifesaver if you need more custom menus for widget areas.
+ *
+ */
+
+// register two additional custom menu slots
+function childtheme_register_menus() {
+    if ( function_exists( 'register_nav_menu' )) {
+        register_nav_menu( 'secondary-menu', 'Secondary Menu' );
+        register_nav_menu( 'tertiary-menu', 'Tertiary Menu' );
+    }
+}
+add_action('thematic_child_init', 'childtheme_register_menus');
+
+/**
+ * Responsive Menu Structure
+ *
+ * Modified to add toggle to label in link format instead of <h3> that is defaulted from the parent theme. This basic structure comes from a
+ *
+ * Reference http://codepen.io/bradfrost/pen/vljdx
+ *
+ */
+
+function childtheme_override_access() {
+    ?>
+    <div id="access" role="navigation">
+        <a class="menu-toggle" href="#">Menu</a>
+        <?php
+        if ( ( function_exists( 'has_nav_menu' ) ) && ( has_nav_menu( apply_filters( 'thematic_primary_menu_id', 'primary-menu' ) ) ) ) {
+            echo  wp_nav_menu(thematic_nav_menu_args());
+        } else {
+            echo  thematic_add_menuclass( wp_page_menu( thematic_page_menu_args () ) );
+        }
+        ?>
+    </div><!-- #access -->
+    <?php
+}
+
+/**
+ * Single Post for Blog
+ *
+ * Shows single pages, instead of a default of 5
+ *
+ */
+
+// show one post per page on homepage
+// http://codex.wordpress.org/Plugin_API/Action_Reference/pre_get_posts
+function childtheme_home_pagesize( $query ) {
+    if ( is_admin() || ! $query->is_main_query() )
+        return;
+    if ( is_home() ) {
+        // Display only 1 post for the original blog archive
+        $query->set( 'posts_per_page', 1 );
+        return;
+    }
+
+}
+add_action( 'pre_get_posts', 'childtheme_home_pagesize', 1 );
+
+/**
+ * Modify Navigational Elements
+ *
+ * The Navigation Above feature of Thematic is pretty silly (and ugly), so that is
+ * completely removed. For the Navigation Below, we remove it on single pages since it
+ * can link to totally unrelated content which doesn't make much sense in most cases.
+ *
+ */
+
+// override completely removes nav above functionality
+function childtheme_override_nav_above() {
+    // silence
+}
+
+function childtheme_override_nav_below() {
+    if (is_single() || is_home()) { ?>
+        <div id="nav-below" class="navigation">
+            <div class="nav-previous"><?php previous_post_link('%link', '<span class="meta-nav"></span> Older Post'); ?> </div>
+            <div class="nav-next"><?php next_post_link('%link', 'Newer Post <span class="meta-nav"></span>'); ?> </div>
+    <?php }
+    else { ?>
+        <div id="nav-below" class="navigation">
+            <div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav"></span> Older Posts', 'thematic')) ?></div>
+            <div class="nav-next"><?php previous_posts_link(__('Newer Posts <span class="meta-nav"></span>', 'thematic')) ?></div>
+    <?php } ?>
+        </div>
+<?php }
+
+
+/**
+ * Thematic Featured Image Size
+ *
+ * Appears on anything with an excerpt set, the default is 100x100 which is ridiculously small, this swaps it out for a more manageable 300x300, but can be easily changed by modifying the sizes.
+ *
+ */
+
+function childtheme_post_thumb_size($size) {
+    $size = array(300,300);
+    return $size;
+}
+add_filter('thematic_post_thumb_size', 'childtheme_post_thumb_size');
+
+/**
+ * Modify the Post Header and Post Footer
+ *
+ * Moves the elemenets and changes structure of the postheader and postfooter to rearrange things for a cleaner look, moves date below post and adds icon fonts in a list style format instead of the default which is just a single line.
+ *
+ * Location
+ */
+
+// kill the post header information, loading this below in the post footer
+function childtheme_override_postheader_postmeta() {
+    // silence!
+}
+
+// example of changing up the display of the entry-utility for a different look
+function childtheme_override_postfooter() {
+    $post_type = get_post_type();
+    $post_type_obj = get_post_type_object($post_type);
+    $tagsection = get_the_tags();
+
+    // Display nothing for "Page" post-type
+    if ( $post_type == 'page' ) {
+        $postfooter = '';
+    // For post-types other than "Pages" press on
+    } else {
+        $postfooter = '<footer class="entry-utility">';
+        $postfooter .= '<ul class="main-utilities">';
+        $postfooter .= '<li class="icon-calendar">' . thematic_postmeta_entrydate() . '</li>';
+        $postfooter .= '<li class="icon-folder">' . thematic_postfooter_postcategory() . '</li>';
+        $postfooter .= '<li class="icon-comment">' . thematic_postfooter_postcomments() . '</li>';
+        if ( $tagsection ) {
+            $postfooter .= '<li class="icon-tag">' . thematic_postfooter_posttags() . '</li>';
+        }
+        if ( is_user_logged_in() ) {
+                $postfooter .= '<li class="icon-pencil">' . thematic_postfooter_posteditlink() . '</li>';
+        }
+        $postfooter .= '</ul>';
+        $postfooter .= "\n\n\t\t\t\t\t</footer><!-- .entry-utility -->\n";
+    }
+    // Put it on the screen
+    echo apply_filters( 'thematic_postfooter', $postfooter ); // Filter to override default post footer
+}
+
+// removes the "Published" from the date in the postfooter, also removes seperator " | "
+function childtheme_override_postmeta_entrydate() {
+    $entrydate = '<span class="entry-date">';
+    $entrydate .= get_the_time(thematic_time_display());
+    $entrydate .= '</span>';
+    return apply_filters('thematic_postmeta_entrydate', $entrydate);
+}
+
+// remove unneeded code from postcategory
+function childtheme_override_postfooter_postcategory() {
+    $postcategory = "\n\n\t\t\t\t\t\t" . '<span class="cat-links">';
+    if (is_single()) {
+        $postcategory .= __('Categories ', 'thematic') . get_the_category_list(', ');
+        $postcategory .= '</span>';
+        $posttags = get_the_tags();
+        if ( !$posttags ) {
+            $postcategory .= '';
+        }
+    } elseif ( is_category() && $cats_meow = thematic_cats_meow(', ') ) {
+        $postcategory .= __('Also posted in ', 'thematic') . $cats_meow;
+        $postcategory .= '</span>' . "\n\n\t\t\t\t\t\t";
+    } else {
+        $postcategory .= __('Posted in ', 'thematic') . get_the_category_list(', ');
+        $postcategory .= '</span>' . "\n\n\t\t\t\t\t\t";
+    }
+    return apply_filters('thematic_postfooter_postcategory',$postcategory);
+}
+
+// remove unneeded code from posttags
+function childtheme_override_postfooter_posttags() {
+    if ( is_single() && !is_object_in_taxonomy( get_post_type(), 'category' ) ) {
+        $tagtext = __('','thematic');
+        $posttags = get_the_tag_list("<span class=\"tag-links\">$tagtext",', ','</span> ');
+    } elseif ( is_single() ) {
+        $tagtext = __('','thematic');
+        $posttags = get_the_tag_list("<span class=\"tag-links\">$tagtext",', ','</span> ');
+    } elseif ( is_tag() && $tag_ur_it = thematic_tag_ur_it(', ') ) {
+        $posttags = '<span class="tag-links">' . __('Also tagged ', 'thematic') . $tag_ur_it . '</span>' . "\n\n\t\t\t\t\t\t";
+    } else {
+        $tagtext = __('','thematic');
+        $posttags = get_the_tag_list("<span class=\"tag-links\">$tagtext",', ','</span>' . "\n\n\t\t\t\t\t\t");
+    }
+    return apply_filters('thematic_postfooter_posttags',$posttags);
+}
+
+
+
+
+
+
+
+// from original
+
+/*// hide unused widget areas inside the WordPress admin
+function childtheme_hide_areas($content) {
+    unset($content['Index Top']);
+    unset($content['Index Insert']);
+    unset($content['Index Bottom']);
+    unset($content['Single Top']);
+    unset($content['Single Insert']);
+    unset($content['Single Bottom']);
+    unset($content['Page Top']);
+    unset($content['Page Bottom']);
+    return $content;
+}
+add_filter('thematic_widgetized_areas', 'childtheme_hide_areas');
+*/
+
+// cuts the default size of the search input field down to cut overlap
+// css sizes this fine, but it could be placed in things other than aside, this is back up. ;)
+function childtheme_thematic_search_form_length() {
+    return "16";
+}
+add_filter('thematic_search_form_length', 'childtheme_thematic_search_form_length');
+
+// change the default search box text
+function childtheme_search_field_value() {
+    return "Search";
+}
+add_filter('search_field_value', 'childtheme_search_field_value');
+
+
+
+
+function childtheme_social_icon_fonts() { ?>
+    <aside id="social" class="aside social">
+        <section>
+            <ul class="social-icons">
+              <li class="social-twitter"><a class="icon-twitter" href="http://twitter.com/usabilitydick" title="Twitter"><span>Twitter</span></a></li>
+              <li class="social-codepen"><a class="icon-codepen" href="http://codepen.io/scottnix" title="Codpen"><span>Codepen</span></a></li>
+              <li class="social-github"><a class="icon-github" href="https://github.com/scottnix" title="Github"><span>Github</span></a></li>
+              <li class="social-rss"><a class="icon-rss" href="http://feeds.feedburner.com/snix" title="RSS"><span>RSS</span></a></li>
+            </ul>
+        </section>
+    </aside>
+<?php }
+add_action('thematic_belowmainasides', 'childtheme_social_icon_fonts');
+
+
+// override functionality of Thematic HTML5 Plugin and Thematic
+// originally H1 on Thematic HTML5 Plugin, originally H3 on Themaitc, changed this to an H4 for fun.
+
+// filter the title opening of widget areas
+function childtheme_before_widgettitle( $content ) {
+    $content = "<h4 class=\"widgettitle\">";
+    return $content;
+}
+add_filter( 'thematic_before_title', 'childtheme_before_widgettitle', 11 );
+
+// filter the title closing of widget area
+function childtheme_after_widgettitle( $content ) {
+    $content = "</h4>\n";
+    return $content;
+}
+add_filter( 'thematic_after_title', 'childtheme_after_widgettitle', 11 );
+
+
+function childtheme_modify_excerpt($text) {
+   return str_replace('[...]', '.... <a href="'.get_permalink().'" class="more-link">Read More &raquo;</a>', $text);
+}
+add_filter('get_the_excerpt', 'childtheme_modify_excerpt');
+
+
+
