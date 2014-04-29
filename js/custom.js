ea39a7c1f35fdafe04eb0b3157c126b11040d71e
Scott Nix <scott@scottnix.com>
2014-04-29 00:04:26 -0700

    Forgot Google Analytics, Hehe

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
+// google analytics
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21469201-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+// mobile menu toggle classes
 jQuery(document).ready(function($) {
     $('.menu-toggle').click(function(e) {
         $('.menu-toggle').toggleClass('toggled');
