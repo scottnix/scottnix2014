dd8b32e5162579c22120b97d22787441115cd0ae
Scott Nix <scott@scottnix.com>
2014-05-30 12:53:44 -0700

    Moving around some jQuery
    Keeping similar things together, being anal

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,3 @@
-// google universal analytics code
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-21469201-1', 'scottnix.com');
-ga('send', 'pageview');
-
-
 // mobile menu toggle classes
 jQuery(document).ready(function($) {
     $('.menu-toggle').click(function(e) {
@@ -16,7 +6,17 @@ jQuery(document).ready(function($) {
     });
 });
 
-// testing some event tracking
+// google universal analytics code
+// support.google.com/analytics/answer/2790010?hl=en&ref_topic=6010376
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-21469201-1', 'scottnix.com');
+ga('send', 'pageview');
+
+// testing some event tracking (through google analytics)
 // developers.google.com/analytics/devguides/collection/analyticsjs/events
 jQuery(document).ready(function($) {
     $('.demo').on('click', function() {
